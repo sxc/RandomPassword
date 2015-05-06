d954de4dc6d094b832a787675141c4f7a7879ce9
Shen Xiaochun <sxc@Macocoa.local>
2015-05-06 10:20:13 +0800

    Initial Commit

---
+++ RandomPassword/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RandomPassword
+//
+//  Created by Shen Xiaochun on 15/5/6.
+//  Copyright (c) 2015年 Sigutian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
